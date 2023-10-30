ca20a0724273ee14f23399efd2b687d157e360a5
Guilherme Gaspar <guilherme.gaspar@tecnico.ulisboa.pt>
2023-10-30 21:21:21 +0100

    test

---
+++ main/main.cpp
@@ -0,0 +1,34 @@
+#include "Texture.h"
+#include "Game.h"
+#include <SDL.h>
+#include <SDL_image.h>
+
+
+int main(int argc, char* argv[]){
+
+    //Game FreeRuben;
+
+    //Start up SDL and create window
+    if(!FreeRuben.init())
+    {
+        printf("Failed to initialize!\n");
+    }
+    else
+    {
+        //Load media
+        if(!FreeRuben.loadMedia())
+        {
+            printf("Failed to load media!\n");
+        }
+        else
+        {   
+            //Run program
+            FreeRuben.run();
+        }
+    }
+
+    //Free resources and close SDL
+    FreeRuben.close();
+
+    return 0;
+}
